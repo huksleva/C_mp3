1b7e1c534e4aefdbb246699bdeb59a4928202c17
huksleva <leonid005xc@gmail.com>
2024-02-06 01:06:35 +0300

    Сделан запуск через консоль cmd с использованием параметров

--- main.c
+++ main.c
@@ -28,54 +28,56 @@ typedef struct {
 } frame; ///фрейм
 int getSize(const char *size) {
     return (size[0] << 21) | (size[1] << 14) | (size[2] << 7) | size[3];
-} ///возвращает размер значения тега или хедера (size)
-int Size_of_char_array(const char *ch) {
-    int ind = 0;
-    while(ch[ind++]!='\0'){}
-    return ind;
-} ///возвращает размер символьного массива
-
+} ///возвращает размер значения в байтах
 void ShowFrames(FILE *f) {
     fseek(f, 0, SEEK_SET);
     ID3v2 header;
+    fread(&header, sizeof(header), 1, f);
+
+
     frame frame;
     unsigned long long count = 10;
-    fread(&header, sizeof(header), 1, f);
 
     while (count < getSize(header.size)) {
         fread(&frame, sizeof(frame), 1, f);
         count = count + sizeof(frame) + getSize(frame.size);
-
-        printf("Parameter: %s\n", frame.id);
-        printf("Value: ");
-        for (int i = 0; i < getSize(frame.size); ++i) {
-            printf("%c", getc(f));
+        if (getSize(frame.size) != 0){
+            printf("Frame: %s\n", frame.id);
+            printf("Value: ");
+            for (int i = 0; i < getSize(frame.size); ++i) {
+                printf("%c", getc(f));
+            }
+            printf("\n\n");
         }
-        printf("\n\n");
     }
-} ///показывает все метаданные mp3 файла
+    printf("\n\n");
+} ///показывает все метаданные файла
 void FindFrame(FILE *f, char *par) {
+    ID3v2 header;
+    fread(&header, sizeof(header), 1, f);
+
     unsigned long long count = 10; //первые 10 байт уже считаны (header)
     fseek(f, 0, SEEK_SET);
-    ID3v2 header;
     frame frame;
-    fread(&header, sizeof(header), 1, f);
+
 
     while (count < getSize(header.size)) {
         fread(&frame, sizeof(frame), 1, f);
         count += sizeof(frame) + getSize(frame.size);
 
         if (strstr(frame.id, par) != NULL) {
-            printf("%s = ", frame.id);
+            printf("%s: ", frame.id);
             for (int i = 0; i < getSize(frame.size); ++i) {
                 printf("%c", getc(f));
             }
             printf("\n");
             return;
         }
-        for (int i = 0; i < getSize(frame.size); ++i){ getc(f); }
+        for (int i = 0; i < getSize(frame.size); ++i){
+            getc(f);
+        }
     }
-    printf("Parameter %s don't exist!\n", par);
+    printf("Frame %s is empty!\n", par);
 } ///показывает значение конкретного тега метаданных
 void ReplaceFrame(FILE *f, char *path, char *par, char *replacement) {
     unsigned long long count = 10; //первые 10 байт уже считаны
@@ -92,24 +94,36 @@ void ReplaceFrame(FILE *f, char *path, char *par, char *replacement) {
     ///записываем метаданные
     while(count < getSize(header.size)){
         fread(&frame, sizeof(frame), 1, f); //считываем
-        fwrite(&frame, sizeof(frame), 1, f2); //записываем
-        count += sizeof(frame) + getSize(frame.size); //прибавляем к счётчику
 
         ///если находим нужный тег
         if (strstr(frame.id, par) != NULL) {
+            fwrite(&frame, sizeof(frame), 1, f2); //записываем
+
             ///записываем в f2 новое значения тега
-            for (int i = 0; i < Size_of_char_array(replacement); ++i){ putc(replacement[i], f2); }
+            for (int i = 0; i < strlen(replacement); ++i){
+                putc(replacement[i], f2);
+            }
             ///пропускаем тег в f
-            for (int i = 0; i < getSize(frame.size); ++i) { getc(f); }
+            for (int i = 0; i < getSize(frame.size); ++i) {
+                getc(f);
+            }
         }
-        else{
-            for (int i = 0; i < getSize(frame.size); ++i) { putc(getc(f), f2); }
+        else {
+            fwrite(&frame, sizeof(frame), 1, f2); //записываем
+            for (int i = 0; i < getSize(frame.size); ++i) {
+                putc(getc(f), f2);
+            }
         }
+        count += sizeof(frame) + getSize(frame.size); //прибавляем к счётчику
     }
+
+
 
     ///теперь дописываем сам аудиофайл
     char buffer;
-    while (fread(&buffer, 1, 1, f) > 0){ putc(buffer, f2); }
+    while (fread(&buffer, 1, 1, f) > 0){
+        putc(buffer, f2);
+    }
 
     ///закрываем все файлы
     fclose(f);
@@ -123,31 +137,32 @@ void ReplaceFrame(FILE *f, char *path, char *par, char *replacement) {
 
 } ///изменяет значение тега
 
-int main() {
+int main(int argc, char **argv) {
+    //printf()
     ///Лабораторная работа №13
-    char s[1000], Parameter[1000], filepath[1000], Value[1000];
+    char Parameter[1000], filepath[1000], Value[1000];
     ///заполняем всё нулями
-    setZero(s, 1000);
     setZero(Parameter, 1000);
     setZero(filepath, 1000);
     setZero(Value, 1000);
-    gets(s);
 
 
     ///строка, в которой ищем; строка, которую ищем; её длина; массив для записи
-    RandW(s, "--filepath=", 11, filepath);
+    RandW(argv[1], "--filepath=", 11, filepath);
     ///для чтения и записи в бинарном режиме
     FILE *f = fopen(filepath, "rb");
 
 
-    if (strstr(s, "--show") != NULL) {
+
+
+    if (strstr(argv[2], "--show") != NULL) {
         ShowFrames(f);
-    } else if (strstr(s, "--set=") != NULL) {
-        RandW(s, "--set=", 6, Parameter);
-        RandW(s, "--value=", 8, Value);
+    } else if ((strstr(argv[2], "--set=") != NULL) && (strstr(argv[3], "--value=") != NULL)) {
+        RandW(argv[2], "--set=", 6, Parameter);
+        RandW(argv[3], "--value=", 8, Value);
         ReplaceFrame(f, filepath, Parameter, Value);
-    } else if (strstr(s, "--get=") != NULL) {
-        RandW(s, "--get=", 6, Parameter);
+    } else if (strstr(argv[2], "--get=") != NULL) {
+        RandW(argv[2], "--get=", 6, Parameter);
         FindFrame(f, Parameter);
     } else {
         printf("You didn't enter anything!");
@@ -156,7 +171,6 @@ int main() {
     fclose(f);
     return 0;
 }
-///Пример вызова функций (их всего 3)
-//app.exe --filepath=Song.mp3 --set=TIT2 --value=ABC
-//app.exe --filepath=Song.mp3 --show
-//app.exe --filepath=Song.mp3 --get=TIT2
+//main.exe --filepath=Song.mp3 --set=TIT2 --value=ABC
+//main.exe --filepath=Song.mp3 --show
+//main.exe --filepath=Song.mp3 --get=TIT2
